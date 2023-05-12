0baaa331ab9f34cb88393a608607782a1826a1fb
Ruan Souza <ruan2014souza17@gmail.com>
2023-05-12 09:13:16 -0300

    visão rapida de JSON Serialization

--- src/main/java/br/com/curso/PersonController.java
+++ src/main/java/br/com/curso/PersonController.java
@@ -19,7 +19,7 @@ import br.com.curso.data.vo.v2.PersonVOV2;
 import br.com.curso.services.PersonService;
 
 @RestController
-@RequestMapping("/api/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 
 	@Autowired
